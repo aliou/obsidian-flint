7ffe933ea0026ee63251b054f530df58f8f44f95
Aliou Diallo <code@aliou.me>
2026-05-29 18:44:34 +0200

    feat: update Flint icon

--- src/icon.ts
+++ src/icon.ts
@@ -1,3 +1,14 @@
 export const FLINT_ICON = `
-<path fill="currentColor" fill-rule="evenodd" d="M20.66 20.66H64.67V50H50V64.67H35.33V79.34H20.66ZM35.33 35.33V50H50V35.33Z" />
-<path fill="currentColor" d="M64.67 50H79.34V79.34H64.67Z" />`;
+<g transform="scale(4.1666667)" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke-width="2" d="M18.8 22 L16.1 21 L13.7 22 L10 20.6 L6.7 18.6 L7 15.3 L6.8 14.2 L6.3 11.7 L4.4 8.3 L5 5.2 L4.8 2 L10 3.7 L12.9 5.9 L17.1 6.7 L18.2 11.7 L18 15.5 L18.4 18 L19.6 20.8 Z" />
+  <path stroke-width="1.6" d="M9.8 3.8 L9.4 6.4" />
+  <path stroke-width="1.6" d="M9.4 6.4 L6.2 7.7 L4.5 8.2" />
+  <path stroke-width="1.6" d="M9.4 6.4 L10.9 7.1 L16.7 6.9" />
+  <path stroke-width="1.6" d="M14.5 10.1 L13.6 13.3 L13.2 16" />
+  <path stroke-width="1.6" d="M13.2 16 L10.4 17.3 L6.8 18.6" />
+  <path stroke-width="1.6" d="M13.2 16 L15.7 16.7 L16.8 17.1" />
+  <path stroke-width="1.4" d="M10.9 7.1 L12.9 8.2 L14.5 10.1" />
+  <path stroke-width="1.4" d="M13.2 16 L12.3 18.6 L11.7 19.9" />
+  <path stroke-width="1.4" d="M13.2 16 L11.2 15 L9.2 14.3" />
+  <path stroke-width="1.2" d="M9.1 13.6 L9.2 15.2" />
+</g>`;
